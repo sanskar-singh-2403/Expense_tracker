f5f87ccf02fea1bb26480d632622a261e20ca79d
ken kaneki <sanskarsinghty1234@gmail.com>
2022-12-15 14:59:50 +0530

    done

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,11 @@
 export default (state,action)=>{
-    if(action.type=='DELETE_TRANSACTION'){
+    if(action.type==='DELETE_TRANSACTION'){
         return({
             ...state,
-            transactions:state.transactions.filter((transaction)=>transaction.id!=action.payload)
+            transactions:state.transactions.filter((transaction)=>transaction.id!==action.payload)
         })
     }
-    else if(action.type=='ADD_TRANSACTION'){
+    else if(action.type==='ADD_TRANSACTION'){
         return({
             ...state,
             transactions:[action.payload,...state.transactions]
